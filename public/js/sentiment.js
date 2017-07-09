b6dc91fa5cac29347efe245b32dd5b1d96225d55
iisaiahwwong <isaiah.wong@jirehsoho.com>
2017-07-09 13:23:25 +0800

    Added new code

--- public/js/sentiment.js
+++ public/js/sentiment.js
@@ -16,14 +16,14 @@ const
     WHITE_THEME = 0xE6EAEB,
     BLACK_THEME = 0x0B141B;
 
-const PARTICLE_SIZE = 20;
+const PARTICLE_SIZE = 100;
 
 
 function init() {
     /** Camera */
     // Initialize THREEjs Camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.5, 10000);
-    camera.position.z = 300;
+    camera.position.z = 600;
     camera.position.y = 0;
 
     /** Get ID of div */
@@ -123,10 +123,35 @@ function animate() {
     render();
     controls.update();
     // stats.end();
+    
+    lookToCameraCSS();
+}
+
+function lookToCameraCSS() {
+
+      if(sceneCss.children.length > 0) {
+        let cssObjects = sceneCss.children;
+
+        for(let i = 0; i < cssObjects.length; i++) {
+
+            cssObject = cssObjects[i];
+
+            if(cssObject instanceof THREE.CSS3DObject) {
+                cssObject.lookAt(camera.position);
+            }
+            
+        }
+
+    }
+
 }
 
 function toRadians(angle) {
     return angle * Math.PI / 180;
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -138,6 +163,15 @@ function toRadians(angle) {
 /* ---------------------------------------------------
     MODELS
 ----------------------------------------------------- */
+
+let emotions = [
+    'POSITIVE', 'NEGATIVE', 'NEUTRAL'
+]
+
+let handle = [
+    'iisaiahwwong', 'alex00i', 'Jasondoe'
+]
+
 function Tweet() {
     
     this.tweet_id;
@@ -147,6 +181,7 @@ function Tweet() {
     this.tweet;
     this.sentiment;
     this.sentiment_value;
+    this.cssLabel
 
 }
 
@@ -156,15 +191,29 @@ function Tweet() {
 
 function initSentimentVisual() {
 
-    intialiseParticleBuffer(10, 50);
-    
-    for(let i = 0; i < 10; i++) {
-        let tweet = new Tweet();
-        tweet.tweet = i+' tweet'
-        tweet.sentiment_value = i;
-        pushData(tweet);
-    }
+    intialiseParticleBuffer(100, 300);
 
+    let index = 0;
+
+    let interval = setInterval(function() {
+        
+        if(index == 100) clearInterval(interval);
+
+        let tweet = new Tweet();
+        tweet.tweet = index+' tweet'
+        tweet.twitter_handle = handle[getRandomInt(0, 2)];
+        tweet.sentiment = emotions[getRandomInt(0, 2)];
+    
+        pushData(tweet);
+
+        index++;
+
+    }, 1000)
+
+
+    // setTimeout(function() {
+    //     spaceOut(10);
+    // }, 2000);
 }
 
 var particles;
@@ -176,6 +225,8 @@ function intialiseParticleBuffer(size, spacing) {
     let positions = new Float32Array(particleCount * 3);
     let colors = new Float32Array(particleCount * 3);
     let sizes = new Float32Array(particleCount);
+    var opacities = new Float32Array(particleCount);
+
     let color = new THREE.Color();
 
     let userData = {};
@@ -192,7 +243,7 @@ function intialiseParticleBuffer(size, spacing) {
         let phi = THREE.Math.randFloatSpread(360);
 
         vertex.x = spacing * Math.sin(theta) * Math.cos(phi);
-        vertex.y = spacing * Math.sin(theta) * Math.sin(phi);
+        vertex.y = spacing * 1.1 * Math.sin(theta) * Math.sin(phi);
         vertex.z = spacing * Math.cos(theta);
 
         vertex.toArray(positions, i * 3);
@@ -202,20 +253,24 @@ function intialiseParticleBuffer(size, spacing) {
         sizes[i] = PARTICLE_SIZE;
         userData[i] = '';
 
+        opacities[i] = 0;
+
     }
 
     geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
     geometry.addAttribute('customColor', new THREE.BufferAttribute(colors, 3));
     geometry.addAttribute('size', new THREE.BufferAttribute(sizes, 1));
+    geometry.addAttribute('opacity', new THREE.BufferAttribute(opacities, 1));
 
     material = new THREE.ShaderMaterial({
         uniforms: {
             color: { value: new THREE.Color(0xffffff) },
-            texture: { value: new THREE.TextureLoader().load("/textures/sprites/disc.png") }
+            texture: { value: new THREE.TextureLoader().load("/textures/sprites/node 12.11.18 PM.png") }
         },
         vertexShader: document.getElementById('vertexshader').textContent,
         fragmentShader: document.getElementById('fragmentshader').textContent,
-        alphaTest: 0.9
+        alphaTest: 0.9,
+        transparent: true,
     });
 
     particles = new THREE.Points(geometry, material);
@@ -223,6 +278,23 @@ function intialiseParticleBuffer(size, spacing) {
 
     sceneGL.add(particles);
 
+}
+
+function spaceOut(space) {
+    let geometry = particles.geometry;
+    let attributes = geometry.attributes;
+    let index;
+
+    for (let i = 0; i < attributes.position.array.length; i++) {
+
+        index = i * 3;
+
+        attributes.position.array[index] *= space;
+        attributes.position.array[index + 1] *= space;
+        attributes.position.array[index + 2] *= space;
+        attributes.position.needsUpdate = true;
+        
+    }
 }
 
 
@@ -241,9 +313,9 @@ function changeOpacity(material, opacity) {
     DATA STREAM
 ----------------------------------------------------- */
 
-function pushData(object) {
+function pushData(tweet) {
 
-    if(!(object instanceof Tweet)) return;
+    if(!(tweet instanceof Tweet)) return;
 
     let userData = particles.userData;
 
@@ -252,25 +324,70 @@ function pushData(object) {
     for(let key in userData) {
         
         if(userData[key] instanceof Tweet) continue;
-        userData[key] = object;
+        userData[key] = tweet;
 
         let geometry = particles.geometry;
         let attributes = geometry.attributes;
 
         let index = key * 3;
 
-        let colorHex = (object.sentiment_value > 5) ? 0xff0000 : 0xffffff;
+        let colorHex = processSentiment(tweet);
 
         color.set(colorHex);
+
+        let x = attributes.position.array[index];
+        let y = attributes.position.array[index + 1];
+        let z = attributes.position.array[index + 2];
+
+        let cssLabel = addLabel(new THREE.Vector3(x, y, z), tweet, 'tweet', 60);
+        tweet.cssLabel = cssLabel
 
         attributes.customColor.array[index] = color.r;
         attributes.customColor.array[index + 1] = color.g;
         attributes.customColor.array[index + 2] = color.b;
         attributes.customColor.needsUpdate = true;
 
+        attributes.opacity.array[key] = 1;
+        attributes.opacity.needsUpdate = true;
+
         break;
 
     }
+}
+
+function processSentiment(tweet) {
+
+    if(! (tweet instanceof Tweet)) return;
+    
+    sentiment = tweet.sentiment.toUpperCase();
+
+    switch(sentiment) {
+        case 'NEGATIVE':
+            return 0xff0000;
+        case 'NEUTRAL':
+            return 0xDAF8FA;
+        case 'POSITIVE':
+            return 0x0ff7ed;
+        default:
+            return 0xDAF8FA;
+    }
+}
+
+function addLabel(bindObject, tweet, cssClass, offset) {
+    
+    let startObject = (bindObject instanceof THREE.Object3D) ? bindObject.position : bindObject;
+	
+	var label = document.createElement('div');
+	label.className = cssClass;
+	label.innerHTML = tweet.tweet;
+	
+	var object = new THREE.CSS3DObject(label);
+	object.position.x = startObject.x + offset;
+	object.position.y = startObject.y;
+	object.position.z = startObject.z;
+	
+	return object;
+    
 }
 
 
@@ -303,7 +420,7 @@ function onDocumentMouseMove(event) {
 
             INTERSECTED = intersects[0].index;
 
-            attributes.size.array[INTERSECTED] = 10 * 4.25;
+            attributes.size.array[INTERSECTED] = PARTICLE_SIZE * 2.25;
             attributes.size.needsUpdate = true;
 
         }
@@ -318,6 +435,8 @@ function onDocumentMouseMove(event) {
     }
 }
 
+let tempLabel;
+
 function onDocumentMouseDown(event) {
 
     setUpRaycaster(event);
@@ -325,12 +444,19 @@ function onDocumentMouseDown(event) {
     var intersects = raycaster.intersectObject(particles);
 
     if (intersects.length > 0) {
+
+        removeCSSObject(tempLabel);
+
         INTERSECTED = intersects[0].index;
-        console.log(INTERSECTED);
+        tempLabel = particles.userData[INTERSECTED].cssLabel;
+        sceneCss.add(tempLabel);
+
         console.log(particles.userData[INTERSECTED]);
     }
     else { // No interaction
+
         INTERSECTED = null;
+
     }
 }
 
@@ -344,4 +470,13 @@ function setUpRaycaster(event) {
     // console.log('Y | ' + mouse.y);
 
     raycaster.setFromCamera(mouse, camera);
+}
+
+function removeCSSObject(label) {
+    if(!label) return;
+
+    if(typeof label.length !== 'undefined') 
+        if(label.length > 0) return;
+
+    sceneCss.remove(sceneCss.getObjectById(label.id));
 }
